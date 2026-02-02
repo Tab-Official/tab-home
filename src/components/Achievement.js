8510972458267b4d91b45864da2dc94d8a895ec4
jeongbyeongjoo <jbj030108@icloud.com>
2026-02-02 23:23:33 +0900

    feat: 2025년도  수상경력 업데이트

--- src/components/Achievement.js
+++ src/components/Achievement.js
@@ -146,6 +146,29 @@ export default function Achievement() {
             </div>
             <hr />
           </li>
+
+          <li>
+
+            <div className="timeline-middle">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+            </div>
+            <div className="timeline-start md:text-end mb-10">
+              <time className="font-mono italic">2025</time>
+              <div>
+                <div>
+                  한국자동차공학회장상 수상
+                </div>
+                <div>
+                  제14회 한국외대 총장배 이공계학술제 대상 수상
+                </div>
+                <div>
+                  용인세브란스병원 디지털 헬스케어 해커톤 최우수상 수상
+                </div>
+              </div>
+            </div>
+            <hr />
+          </li>
+
         </ul>
 
 
